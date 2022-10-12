7ac1df3ed6bd05d4669b7d4b5e9535b99ad04bd0
yanshiyao <ysy504933488@gmail.com>
2022-10-12 15:11:44 +0800

    改进了算法

--- new.py
+++ new.py
@@ -8,7 +8,15 @@ import pandas as pd
 import xlwt
 from sklearn.datasets import load_wine
 
-sheetname = ['0', './list1.xls', './list2.xls', './list3.xls', './list4.xls', './list5.xls']
+sheetname = ['0']
+
+
+for i in range(1,5000):
+    s = ''
+    s+='./list'
+    s+=str(i)
+    s+='.xls'
+    sheetname.append(s)
 
 
 def loadxls(x):
@@ -58,7 +66,30 @@ def stu_judge(absence, j):
         flag = 1
     return (flag, cnt)
 
-
+def stu_judgecge(absence,j,cnt,trans_count):
+    count = 0
+    ecount = 0
+    flag = 0
+    if j + 3 < 23:
+        if absence[j + 3] == 0:
+            ecount = ecount + 1
+            flag = 1
+        count += 1
+    if j + 4 < 23:
+        if absence[j + 4] == 0:
+            ecount = ecount + 1
+            flag = 1
+        count += 1
+    if flag==1:
+        k = 5
+        cnt +=1
+        trans_count += 1
+        while j + k < 23:
+            if absence[j+k]==0:
+                ecount += 1
+            count +=1
+            k += 1
+    return cnt,count,ecount,trans_count
 def call_once(stu):
     i = 0
     j = 0  # 第j次课
@@ -72,58 +103,25 @@ def call_once(stu):
     while j < 20:
         j=j+1
         k = i
-        i = i + 45
+        i = i + 11
         while k < i and k < 90:
             if stu[k][j + 2] == 0:
                 absense.append([])
                 absense[absense_count] = stu[k]
-                absense_count = absense_count + 1
                 ecount = ecount + 1
+                cnt,ccount,ec,truance_count=stu_judgecge(absense[absense_count],j,cnt,truance_count)
+                absense_count = absense_count + 1
+                ecount += ec
+                count += ccount
             k = k + 1
-        count = count + 45
-
-        if float(absense_count / i) > 6.5 / 90:
-            while cnt < absense_count:
-                flag, ec = stu_judge(absense[cnt], j)
-                if flag == 1:
-                    truance.append([])
-                    truance[truance_count] = absense[cnt]
-                    truance_count = truance_count + 1
-                    ecount = ecount + ec
-                count = count + 2
-                cnt = cnt + 1
-            j=j+2
-            if float(truance_count / i) > 5.0 / 90:
+        count = count + 11
+        if float(truance_count / k) > float(1.9 / 90):
                 break
-        if i==90:
-            while cnt < absense_count:
-                flag, ec = stu_judge(absense[cnt], j)
-                if flag == 1:
-                    truance.append([])
-                    truance[truance_count] = absense[cnt]
-                    truance_count = truance_count + 1
-                    ecount = ecount + ec
-                count = count + 2
-                cnt = cnt + 1
-            j=j+2
-            break
-    while j<20:
-        j=j+1
-        k=0
-        while k<truance_count:
-            if truance[k][j+2]==0:
-                ecount=ecount+1
-            count=count+1
-            k=k+1
-    print(ecount)
-    print(count)
-    E = ecount/count
-    print('E=', E)
     return (ecount,count)
 
 sum_count = 0  # 总点名次数
 sum_ecount = 0  # 总有效点名次数
-for i in range(1,6):
+for i in range(1,5000):
     stu = []
     stu = loadxls(i)
     se,sc=call_once(stu)
@@ -131,7 +129,7 @@ for i in range(1,6):
     sum_ecount+=se
     sum_count+=sc
 
-# print(sum_ecount)
-# print(sum_count)
-# E = sum_ecount/sum_count
-# print('E=', E)
+print(sum_ecount)
+print(sum_count)
+E = sum_ecount/sum_count
+print('endE=', E)
